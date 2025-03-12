eaade40d6450ee728ec990214f2479e4fd07926b
shivamm-5106 <shivampandey5106@gmail.com>
2025-03-12 16:59:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import KietMun from "./CommitteesPage";
+// import  from "./CommitteesSection";
+// ("./CommitteesSection");
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold text-blue-600 mb-6">
+        Welcome to KIET MUN
+      </h1>
+      <KietMun />
+    </div>
+  );
+}
+``;
